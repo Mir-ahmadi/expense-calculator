4830a848e94e67dfa621f6015919281b6cf3625b
Mir-Ahmadi <mohammad.mir1995@gmail.com>
2021-12-19 20:55:45 +0330

    -add submit process to project -add table to project

--- Master.js
+++ Master.js
@@ -1,4 +1,32 @@
-function changeText()
-{
-    document.getElementById("p1").innerHTML= "GoodBye Bro!";
+let expenseList = [];
+
+function submit() {
+  let result = { title: "", amount: 0, id: 0 };
+  const amountStr = document.getElementById("amount").value;
+  const amount = parseFloat(amountStr);
+  const plusMinus = document.getElementById("plus-minus").value;
+  const title = document.getElementById("title").value;
+  if (plusMinus == "-") {
+    result.amount = amount * -1;
+  } else {
+    result.amount = amount * 1;
+  }
+  result.id = expenseList.length;
+  result.title = title;
+  expenseList.push(result);
+  addRow(result.title, result.amount, result.id);
+}
+
+function addRow(title, amount, id) {
+  let result;
+  const tableBody = document.getElementById("expense-table-body").innerHTML;
+  result = `\n          <tr>\n            <td>${amount}</td>\n            <td>${title}</td>\n            <td>${id}</td>\n          </tr>\n        `;
+  document.getElementById("expense-table-body").innerHTML = tableBody + result;
+  console.log(tableBody);
+}
+
+function renderTableRows(expenseList) {
+  for (const row of expenseList) {
+    addRow(row.title, row.amount, row.id);
+  }
 }
